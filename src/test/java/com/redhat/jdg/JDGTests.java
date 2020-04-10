9084fe2e8417448e8cc1ab5a7e3119aa6ac0c3ca
abarbosa <abarbosa@localhost.localdomain>
2020-04-10 14:42:20 -0500

    update add cache configuration

--- src/test/java/com/redhat/jdg/JDGTests.java
+++ src/test/java/com/redhat/jdg/JDGTests.java
@@ -1,34 +1,36 @@
 package com.redhat.jdg;
 
+import java.util.Date;
+
 import org.infinispan.client.hotrod.RemoteCache;
 import org.infinispan.client.hotrod.RemoteCacheManager;
 import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
-import org.infinispan.client.hotrod.configuration.NearCacheMode;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.IOException;
-import java.util.Date;
-import java.util.logging.Logger;
-
-import static org.junit.Assert.*;
-
 public class JDGTests {
 
-    private static final String TEST_CACHE_KEY = "testCache";
+    private static final String TEST_CACHE_NAME = "testCache";
     private static final String TEST_KEY = "testKey1";
     private static final String TEST_VALUE = "testValue" + new Date();
-
+    
+    private static final String HOST1 = "127.0.0.1";
+    private static final int PORT1 = 11222; 
+    private static final String HOST2 = "127.0.0.1";
+    private static final int PORT2 = 11322; 
+    
     RemoteCacheManager manager;
 
     @Before
     public void connect() {
         ConfigurationBuilder builder = new ConfigurationBuilder();
         builder.connectionPool().maxActive(5).minIdle(3).maxWait(60000)
-        .addServers("127.0.0.1:11222");
-        //.addServer().host("127.0.0.1").port(11222);
-
+        .addServer().host(HOST1).port(PORT1)
+        .addServer().host(HOST2).port(PORT2)
+        .validate()
+        ;
+        
         manager = new RemoteCacheManager(builder.build(), true);
     }
 
@@ -39,26 +41,26 @@ public class JDGTests {
 
     @Test
     public void testPut() {
-        RemoteCache<Object, Object> cache = getCache(TEST_KEY);
+        RemoteCache<String, String> cache = getCache(TEST_CACHE_NAME);
         cache.put(TEST_KEY, TEST_VALUE);
         
-        final Object value = cache.get(TEST_KEY);
+        final String value = cache.get(TEST_KEY);
         System.out.println("Get the value: "+ value);
         
     }
 
-    private RemoteCache<Object, Object> getCache(String testKey) {
-    	System.out.println("Requesting cache {}"+ TEST_CACHE_KEY);
-        RemoteCache<Object, Object> cache = manager.getCache(TEST_CACHE_KEY);
-        System.out.println("Got {}"+ cache);
+    private RemoteCache<String, String> getCache(String cacheName) {
+    	System.out.println("Requesting cache: "+ cacheName);
+        RemoteCache<Object, Object> cache = manager.getCache(cacheName);
+        System.out.println("Got: "+ cache);
         
         if(cache==null) {
-            String template = null;
-            System.out.println("Creating cache {}" + TEST_CACHE_KEY);
-            manager.administration().createCache(TEST_CACHE_KEY, template);
-            System.out.println();
+            System.out.println("Creating cache: " + cacheName);
+            String template = "org.infinispan.DIST_SYNC";
+            manager.administration().createCache(cacheName, template);
+            System.out.println("cache "+cacheName+" created");
         }
 
-        return manager.getCache(TEST_CACHE_KEY);
+        return manager.getCache(cacheName);
     }
 }
