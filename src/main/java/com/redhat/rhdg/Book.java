bc2f8e83c70bfa953bfa7cea3301f7e525ff5cd3
Alexander Barbosa <alexanderbarbosaayala@gmail.com>
2023-05-12 17:56:25 -0500

    Basic cache app using JBoss Marshalling

--- src/main/java/com/redhat/rhdg/Book.java
+++ src/main/java/com/redhat/rhdg/Book.java
@@ -1,20 +1,13 @@
 package com.redhat.rhdg;
 
-import org.infinispan.protostream.annotations.ProtoFactory;
-import org.infinispan.protostream.annotations.ProtoField;
+import java.io.Serializable;
 
-public class Book{
+public class Book implements Serializable {
 	
-	@ProtoField(number = 1)
 	String title;
-
-	@ProtoField(number = 2)
 	String description;
-
-	@ProtoField(number = 3, defaultValue= "2023")
 	int publicationYear;	
 	
-	@ProtoFactory
 	Book(String title, String description, int publicationYear) {
 		this.title = title;
 		this.description = description;
